10af6abc8020cb8b637f08b454eebf6ba7d8527d
d3sk3r <erick.mendesoliveira@gmail.com>
2020-08-17 08:07:45 -0300

    default commit

--- i3/.i3/mouse_sens.sh
+++ i3/.i3/mouse_sens.sh
@@ -3,17 +3,19 @@
 # xinput --list --short
 while true; do
         NEW_MOUSEID=$(xinput | grep -i "Mouse" | grep -o -E 'id=[0-9]+' | head -n 1 | cut -d"=" -f2)
-        NEW_MOUSEID2=$(xinput | grep -i "Mouse" | grep -o -E 'id=[0-9]+' | head -n 2 | tail -1 | cut -d"=" -f2)
+        NEW_MOUSEID2=$(xinput | grep -i "Mouse" | grep -o -E 'id=[0-9]+' | head -n 3 | tail -1 | cut -d"=" -f2)
         if [ "$MOUSEID" != "$NEW_MOUSEID" ]; then
                 MOUSEID=$NEW_MOUSEID
                 if [ "$MOUSEID" != "" ]; then
                         xinput --set-prop "$MOUSEID" "libinput Accel Speed" -0.9
+                        echo "1 $MOUSEID"
                 fi
         fi
         if [ "$MOUSEID2" != "$NEW_MOUSEID2" ]; then
                 MOUSEID2=$NEW_MOUSEID2
                 if [ "$MOUSEID2" != "" ]; then
                         xinput --set-prop "$MOUSEID2" "libinput Accel Speed" -0.9
+                        echo "2 $MOUSEID2"
                 fi
         fi
         sleep 2
