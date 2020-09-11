76c873bafada9621c5c2de3aab09eb7f9d6edd9f
d3sk3r <erick.mendesoliveira@gmail.com>
2020-09-11 16:15:08 -0300

    tmux

--- scripts/.scripts/dotfiles.sh
+++ scripts/.scripts/dotfiles.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 function dots(){
-    cd ~/.dotfiles/;
-    git add .;
-    git commit -m "$@";
-    git push;
-    cd ~;
+    cd ~/.dotfiles/
+    git add .
+    git commit -m "$@"
+    git push
+    cd ~
 #    clear
 }
 
