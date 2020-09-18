b3eea1c116761c92b43300dc9a1939d0033121cb
d3sk3r <erick.mendesoliveira@gmail.com>
2020-09-17 21:05:02 -0300

    fixed panic script

--- scripts/.scripts/panic.sh
+++ scripts/.scripts/panic.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-if [ $(playerctl status) = "Playing" ]; then
-    playerctl stop;playerctl previous 
-fi
-mpc -p 1100 seek 0 && mpc -p 1100 pause 
-
-#i3 move scratchpad &
-
-i3 workspace 9 
-sleep 0.1 
-
-urxvt -e gotop -aps &
-urxvt &
-sleep 0.2
-urxvt -e ranger &
-#sleep 0.8 
-
 if xrandr | grep -ow "DP1 connected"; then
-    sleep 0.5
+    #sleep 0.5
     i3 workspace 4
     i3 workspace 9
 fi
+
+i3 workspace 9
+
+if [ $(playerctl status) = "Playing" ]; then
+    playerctl stop;playerctl previous
+fi
+mpc -p 1100 seek 0 && mpc -p 1100 pause
+
+sleep 0.2
+
+urxvt -e gotop -aps &
+#urxvt &
+sleep 0.2
+urxvt -e ranger &
+#sleep 0.8
+
