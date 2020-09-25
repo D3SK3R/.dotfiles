8fb0fc934543a927679272dff6bb16d009afdeb6
d3sk3r <erick.mendesoliveira@gmail.com>
2020-09-24 22:44:19 -0300

    default commit

--- scripts/.scripts/mute-toggle.sh
+++ scripts/.scripts/mute-toggle.sh
@@ -2,11 +2,11 @@
 
 # Check if my audio input (pulseaudio) is muted or not, and send a notification
 
-mute=$(pactl list sources | grep Mute  | sed -n 2p | cut -d : -f2 | sed 's/ //g')
+mute=$(pactl list sources | grep Mute | sed -n 2p | cut -d : -f2 | sed 's/ //g')
 
 if [[ $mute = 'no' ]]; then
     pactl set-source-mute 1 toggle
-    dunstify -t 1000 'Mic Muted' ' ' -p 2 -r 2
+    dunstify -t 1000 'Mic muted' ' ' -p 2 -r 2
 elif [[ $mute = 'yes' ]]; then
     pactl set-source-mute 1 toggle
     dunstify -t 1000 'Mic unmuted' ' ' -p 2 -r 2
