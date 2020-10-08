4a79cd39956bb877f29a48f91fe278cd30d999f6
d3sk3r <erick.mendesoliveira@gmail.com>
2020-10-08 19:16:22 -0300

    default commit

--- scripts/.scripts/hotkeys.sh
+++ scripts/.scripts/hotkeys.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-awk '/^[a-z]|{/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc | column -t -s $'\t' | rofi -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15 -yoffset 0
+awk '/^[a-zA-Z]|{/ && last {print "<small>",$0,"\t",last,"</small>"} {last=""} /^#/{last=$0}' ~/.config/sxhkd/sxhkdrc |
+    column -t -s $'\t' |
+    rofi -dmenu -i -markup-rows -no-show-icons -width 1000 -lines 15 -yoffset 0
