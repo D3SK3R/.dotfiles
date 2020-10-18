dd1aee48b61ae91c00a8089dc91d29f858b745fb
d3sk3r <erick.mendesoliveira@gmail.com>
2020-10-18 18:50:25 -0300

    default commit

--- scripts/.scripts/brightness.sh
+++ scripts/.scripts/brightness.sh
@@ -3,6 +3,9 @@
 # USAGE:
 # ./brightness + or - to increase or decrease brightness, or no argument to show brightness
 
+# Use this command if you get a permission error
+#sudo chmod a+rw /sys/class/backlight/intel_backlight/brightness
+
 # command to know what file to use: ls /sys/class/backlight
 # to be able to edit the file:
 # sudo chmod 666 /sys/class/backlight/intel_backlight/brightness
@@ -14,6 +17,10 @@ if [[ "$#" -eq 0 ]]; then
     cat $file
 elif [[ "$1" = "-" ]]; then
     echo $(( level - 1 )) > $file
+    current=$(cat $file)
+    dunstify -t 1000 "Brightness -1%" "Current brightness: $current" -p 2 -r 2
 elif [[ "$1" = "+" ]]; then
     echo $(( level + 1 )) > $file
+    current=$(cat $file)
+    dunstify -t 1000 "Brightness +1%" "Current brightness: $current" -p 2 -r 2
 fi
--- scripts/.scripts/brightnessUP.sh
+++ scripts/.scripts/brightnessUP.sh
@@ -8,8 +8,8 @@
 # yay xorg-xbacklight
 xbacklight -inc 1
 
-# Option 3: BEST if it worked...
+# Option 3: BEST, way faster...
 #sh ~/.scripts/brightness.sh +
 
-current=$(light -G)
-dunstify -t 1000 "Brightness +1%" "Current brightness: $current" -p 2 -r 2
+#current=$(light -G)
+dunstify -t 1000 "Brightness +1%" "Current brightness: $(light -G)" -p 2 -r 2
