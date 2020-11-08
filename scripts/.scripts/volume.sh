8b386adc02f9c3e43928fac036a778521306f040
d3sk3r <erick.mendesoliveira@gmail.com>
2020-11-08 18:07:36 -0300

    default commit

---
+++ scripts/.scripts/volume.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Changes the volume level of an audio track
+# Signals an the status bar to update
+
+if [ "$1" = "All" ]; then
+    if [ "$2" = "mute" ]; then
+        volume="$(i3volume)"
+
+        if [ ! "$(echo "$volume" | grep -q "mute")" ]; then
+            amixer set "Master" "mute"
+            amixer set "Headphone" "mute"
+            amixer set "Speaker" "mute"
+        else
+            amixer set "Master" "unmute"
+            amixer set "Headphone" "unmute"
+            amixer set "Speaker" "unmute"
+        fi
+    fi
+else
+    amixer set "$1" "$2"
+    current=$(pacmd list-sinks | grep -A 15 "index: " | grep 'volume:' | grep -E -v 'base volume:' | awk -F : '{print $3}' | grep -o -P '.{0,3}%'| sed s/.$// | tr -d ' ')
+
+    dunstify -t 1000 "Volume $2" "Current volume: $current" -p 1 -r 1
+
+fi
+
+polybar-msg hook volume 1
--- scripts/.scripts/volumeDOWN.sh
+++ scripts/.scripts/volumeDOWN.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-amixer -D pulse sset Master 2%-
+#amixer -D pulse sset Master 2%-
+amixer set Master 2%-
 
 #current=$(pacmd list-sinks | grep -A 15 "index: 0" | grep 'volume:' | grep -E -v 'base volume:' | awk -F : '{print $3}' | grep -o -P '.{0,3}%'| sed s/.$// | tr -d ' ')
 
