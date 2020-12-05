6b0e8744d6ee7fd95513f41d16fdf7e5f4842d6e
d3sk3r <erick.mendesoliveira@gmail.com>
2020-12-05 15:05:12 -0300

    default commit

--- scripts/.scripts/mount.sh
+++ scripts/.scripts/mount.sh
@@ -9,24 +9,24 @@ mounted=$(lsblk -lp | grep "part /" | awk '{print $1, "(" $4 ")"}' | tail -n +3)
 #[[ "$mounted" = "" ]] && echo 'Nothing to unmount' && exit
 
 opts="Mount\nUnmount"
-opts=$(echo -e $opts | rofi -dmenu -i -mesg "Mount or Unmount Drives" | awk '{print $1}')
+opts=$(echo -e $opts | rofi -dmenu -i -theme themes/mount-material.rasi -mesg "Mount or Unmount Drives" | awk '{print $1}')
 [[ "$opts" = "" ]] && echo 'Nothing to selected' && exit
 
 if [ $opts = "Mount" ]; then
 
-    chosen=$(echo "$mountable" | rofi -dmenu -i -mesg "Drive to mount" | awk '{print $1}')
+    chosen=$(echo "$mountable" | rofi -dmenu -i -theme themes/mount-material.rasi -mesg "Drive to mount" | awk '{print $1}')
     [[ "$chosen" = "" ]] && echo 'Nothing selected' && exit
     #gksu mount "$chosen" && exit
 
     # optional mount points
     dirs=$(find /mnt /media /mount -type d -maxdepth 3 2>/dev/null)
-    mountpoint=$(echo "$dirs" | rofi -dmenu -i -mesg "Mount point")
+    mountpoint=$(echo "$dirs" | rofi -dmenu -i -theme themes/mount-material.rasi -mesg "Mount point")
     [[ "$mountpoint" = "" ]] && echo 'Nothing selected' && exit
 
     #mountpoint=/mnt/usb
 
     if [[ ! -d "$mountpoint" ]]; then
-        mkdiryn=$(echo -e "No\nYes" | rofi -dmenu -i -mesg "$mountpoint does not exist. Create it?")
+        mkdiryn=$(echo -e "No\nYes" | rofi -dmenu -i -theme themes/mount-material.rasi -mesg "$mountpoint does not exist. Create it?")
         if [[ "$mkdiryn" = Yes ]];then
             gksu mkdir "$mountpoint"
         else
@@ -37,7 +37,7 @@ if [ $opts = "Mount" ]; then
     gksu mount $chosen $mountpoint && dunstify "$chosen mounted to $mountpoint"
     #dunstify "$chosen mounted to $mountpoint"
 else
-        chosen=$(echo "$mounted" | rofi -dmenu -i -mesg "Drive to umount" | awk '{print $1}')
+        chosen=$(echo "$mounted" | rofi -dmenu -i -theme themes/mount-material.rasi -mesg "Drive to umount" | awk '{print $1}')
     [[ "$chosen" = "" ]] && echo 'Nothing selected' && exit
     #gksu mount "$chosen" && exit
 
--- scripts/.scripts/rofi_power.sh
+++ scripts/.scripts/rofi_power.sh
@@ -5,16 +5,18 @@
 
 # 2016 Oliver Kraitschy - http://okraits.de
 
+uptime=$(uptime -p | sed -e 's/up //g')
+
 #OPTIONS="Reboot system\nPower-off system\nSuspend system\nHibernate system"
 #OPTIONS=" Lock\n Sleep\n Logout\n Restart\n Shutdown"
-OPTIONS=" LOCK\n\n SLEEP\n\n LOGOUT\n\n REBOOT\n\n SHUTDOWN"
+OPTIONS=" LOCK\n SLEEP\n LOGOUT\n REBOOT\n SHUTDOWN"
 
 # source configuration or use default values
 if [ -f $HOME/.config/rofi-power/config ]; then
   source $HOME/.config/rofi-power/config
 else
   #LAUNCHER="rofi -width 30 -dmenu -i -p rofi-power:"
-  LAUNCHER="rofi -dmenu -i -f 'DejaVu Sans Mono 12'"
+  LAUNCHER="rofi -dmenu -i -f 'DejaVu Sans Mono 12' -theme themes/powermenu-material.rasi"
   USE_LOCKER="false"
   LOCKER="/home/d3sk3r/.i3/lock.sh"
 fi
@@ -24,7 +26,7 @@ if [ ${#1} -gt 0 ]; then
   OPTIONS="Exit window manager\n$OPTIONS"
 fi
 
-option=`echo -e $OPTIONS | $LAUNCHER | awk '{print $2}' | tr -d '\r\n'`
+option=`echo -e $OPTIONS | $LAUNCHER -mesg "uptime: $uptime" | awk '{print $2}' | tr -d '\r\n'`
 if [ ${#option} -gt 0 ]
 then
     case $option in
