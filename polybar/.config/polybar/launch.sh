cae25f10298f994be190169f4865e63eceb3c8b0
d3sk3r <erick.mendesoliveira@gmail.com>
2020-12-28 14:52:43 -0300

    default commit

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -32,7 +32,7 @@ case $desktop in
     if type "xrandr" > /dev/null; then
         MONITOR=$PRIMARY polybar --reload main -c ~/.config/polybar/config &
         # sleep necessary so that the bar on the main monitor loads first and takes the tray output
-        sleep 0.5 
+        sleep 1
         for m in $OTHERS; do
             MONITOR=$m polybar --reload main -c ~/.config/polybar/config &
         done
