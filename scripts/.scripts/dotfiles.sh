b68df52168588e13856aec14228a51e57e4c4a38
d3sk3r <erick.mendesoliveira@gmail.com>
2021-01-15 13:28:27 -0300

    default commit

--- scripts/.scripts/dotfiles.sh
+++ scripts/.scripts/dotfiles.sh
@@ -12,18 +12,3 @@ git push
 cd ~
 clear
 
-
-# As a function to be sourced in .bashrc/.zshrc
-#function dots(){
-#    message=$@
-#    if [ -z "$message" ]; then
-#        message="default commit"
-#    fi
-#    cd ~/.dotfiles/
-#    git add .
-#    git commit -m "$message"
-#    git push
-#    cd ~
-#    clear
-#}
-
