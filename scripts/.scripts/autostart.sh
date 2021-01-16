7ae419fcb18d73d90f976de081d8e853abc6f134
d3sk3r <erick.mendesoliveira@gmail.com>
2021-01-16 16:22:43 -0300

    default commit

--- scripts/.scripts/autostart.sh
+++ scripts/.scripts/autostart.sh
@@ -3,10 +3,10 @@
 # check if the script is running right after the system started
 # so that I can edit and reload my bspwmrc without it
 # re-running this script
-uptime="$(cat /proc/uptime | cut -d' ' -f1 | cut -d'.' -f1)"
-if [[ "$uptime" -gt "100" ]]; then
-    exit 
-fi
+#uptime="$(cat /proc/uptime | cut -d' ' -f1 | cut -d'.' -f1)"
+#if [[ "$uptime" -gt "100" ]]; then
+#    exit 
+#fi
 
 dunstify "Running autostart script"
 
@@ -21,22 +21,29 @@ function run {
 #######################
 ###     scripts     ###
 #######################
-$HOME/.config/polybar/launch.sh &
+# everything about monitors is commented
+# since I'm using autorandr that executed all of this
+#$HOME/.config/bspwm/monitors-bspwm &
+
+# both scripts are already inside monitors-bspwm
+#$HOME/.scripts/monitors.sh &
+
+sleep 3 && $HOME/.config/polybar/launch.sh &
 
 $HOME/.scripts/mouse_sens.sh &
 
-#$HOME/.scripts/monitors.sh &
-
 $HOME/.config/bspwm/swallowbspwm &
 
-#sleep 5 && $HOME/.scripts/headset-mic.sh &
-
 #$HOME/.scripts/better.sh &
+
+#sleep 2 && $HOME/.scripts/headset-mic.sh &
 
 #######################
 ###     programs    ###
 #######################
 xrdb -merge $HOME/.Xresources
+
+autorandr --change &
 
 run sxhkd
 
@@ -58,7 +65,7 @@ run clipster -d
 
 run copyq 
 
-#run xautolock -time 60 -locker ~/.scripts/lock.sh
+run xautolock -time 60 -locker ~/.scripts/lock.sh
 
 run ff-theme-util
 
@@ -75,6 +82,8 @@ setxkbmap -model abnt2 -layout br -variant abnt2 &
 
 nitrogen --restore;sleep 1;picom -b --experimental-backend &
 
+flashfocus &
+
 # floating stuff
 sleep 3 && urxvt -name floating_terminal &
 
@@ -82,10 +91,8 @@ sleep 3 && urxvt -name floating_terminal2 &
 
 #sleep 3 && spotify &
 
-sleep 4 && firefox &
+#sleep 4 && firefox &
 
-sleep 3 && discord & premid &
-
-flashfocus &
+#sleep 3 && discord & premid &
 
 
