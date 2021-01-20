20607aa53d38060ffb44773d24c2191728d380b6
d3sk3r <erick.mendesoliveira@gmail.com>
2021-01-20 04:55:29 -0300

    default commit

--- scripts/.scripts/autostart.sh
+++ scripts/.scripts/autostart.sh
@@ -85,7 +85,7 @@ setxkbmap -model abnt2 -layout br -variant abnt2 &
 
 nitrogen --restore;sleep 1;picom -b --experimental-backend &
 
-sleep 3 && flashfocus &
+sleep 4 && flashfocus &
 
 # floating stuff
 sleep 3 && urxvt -name floating_terminal &
