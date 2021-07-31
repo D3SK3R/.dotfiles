1733b53f7410fa14c9538f0e18531e0959cb46d1
d3sk3r <erick.mendesoliveira@gmail.com>
2021-07-31 13:23:36 -0300

    default commit

--- scripts/.scripts/cleanup.sh
+++ scripts/.scripts/cleanup.sh
@@ -6,7 +6,7 @@
 # sudo pacman -Rns $(pacman -Qtdq): Remove all unused dependencies from unisntalled packages
 # sudo paccache -r: clean all packages, except the 3 most recent versions
 
-read -p "This command will remove cached packages and sync database of not installed packages, keep only the 3 most recent versions of installed packages, and remove all unused dependencies...
+read -p "This command will remove cached packages and sync database of not installed packages, keep only the 3 most recent versions of installed packages, and remove all unused dependencies... It will also clean the trash.
 [Y/n]: " choice 
 
 if [ "$choice" = "Y" ] || [ "$choice" = "y" ] || [ -z "$choice" ]; then
@@ -16,6 +16,7 @@ if [ "$choice" = "Y" ] || [ "$choice" = "y" ] || [ -z "$choice" ]; then
     yay -Yc &&
     sudo pacman -Rns --noconfirm $(pacman -Qtdq) && 
     sudo paccache -r
+    rm -rf ~/.local/share/Trash/files/*
 else
     echo 'Aborting...'
 fi
