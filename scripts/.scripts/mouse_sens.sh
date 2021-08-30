305fcb3b4061172bbb42f04e4ad896e483cbeb6a
d3sk3r <erick.mendesoliveira@gmail.com>
2021-08-30 18:25:31 -0300

    default commit

--- scripts/.scripts/mouse_sens.sh
+++ scripts/.scripts/mouse_sens.sh
@@ -7,14 +7,14 @@ while true; do
         if [ "$MOUSEID" != "$NEW_MOUSEID" ]; then
                 MOUSEID=$NEW_MOUSEID
                 if [ "$MOUSEID" != "" ]; then
-                        xinput --set-prop "$MOUSEID" "libinput Accel Speed" -0.9
+                        xinput --set-prop "$MOUSEID" "libinput Accel Speed" -0.8
                         echo "1 $MOUSEID"
                 fi
         fi
         if [ "$MOUSEID2" != "$NEW_MOUSEID2" ]; then
                 MOUSEID2=$NEW_MOUSEID2
                 if [ "$MOUSEID2" != "" ]; then
-                        xinput --set-prop "$MOUSEID2" "libinput Accel Speed" -0.9
+                        xinput --set-prop "$MOUSEID2" "libinput Accel Speed" -0.8
                         echo "2 $MOUSEID2"
                 fi
         fi
