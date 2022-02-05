a09d9207f5a80f2a74b0b4a7096cff9027766f7c
d3sk3r <erick.mendesoliveira@gmail.com>
2022-02-05 02:27:45 -0300

    default commit

--- scripts/.scripts/pipResize.sh
+++ scripts/.scripts/pipResize.sh
@@ -1,17 +1,24 @@
 #!/bin/sh
 
 # make a window picture-in-picture size
-# and move it to the top-right corner
+# and move it to the top-right corner.
+# execute again hovering the same window
+# and it will disable the pip mode
 
 node="$(bspc query -N -n ${1:-focused})" || exit 1
 wid="$(xdotool getwindowfocus)" || exit 1
+state=$(bspc query -n $node --tree | jq -r '.client["state"]') || exit 1
+#desktop="$(bspc query -D -d focused --names)"
 
-echo $node
-echo $wid
+if [[ "$state" == "tiled" ]]; then
+    bspc node $node -t floating --flag sticky=on
 
-bspc node $node -t floating
+    xdotool windowsize $wid 302 170 &
 
-xdotool windowsize $wid 302 170 &
+    xdotool windowmove $wid 2424 64 &
+else
+    bspc node $node -d 2
 
-xdotool windowmove $wid 2424 64 &
+    bspc node $node -t tiled --flag sticky=off
+fi
 
