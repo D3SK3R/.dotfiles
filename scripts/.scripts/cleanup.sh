c5fc8b64db6b656cc6129f49298d7f7f521afffc
d3sk3r <erick.mendesoliveira@gmail.com>
2022-03-31 04:27:00 -0300

    default commit

--- scripts/.scripts/cleanup.sh
+++ scripts/.scripts/cleanup.sh
@@ -6,7 +6,7 @@
 # sudo pacman -Rns $(pacman -Qtdq): Remove all unused dependencies from unisntalled packages
 # sudo paccache -r: clean all packages, except the 3 most recent versions
 
-read -p "This command will remove cached packages and sync database of not installed packages, keep only the 3 most recent versions of installed packages, and remove all unused dependencies... It will also clean the trash.
+read -p "This command will remove cached packages and sync database of not installed packages, keep only the 2 most recent versions of installed packages (paccache), and remove all unused dependencies... It will also clean the trash.
 [Y/n]: " choice 
 
 if [ "$choice" = "Y" ] || [ "$choice" = "y" ] || [ -z "$choice" ]; then
@@ -24,12 +24,25 @@ if [ "$choice" = "Y" ] || [ "$choice" = "y" ] || [ -z "$choice" ]; then
     #printf "\nsudo pacman -Rns --noconfirm $(pacman -Qtdq)"
     #sudo pacman -Rns --noconfirm $(pacman -Qtdq) && 
 
-    printf "\nsudo paccache -r\n"
+    printf "\nsudo paccache -r -k 2\n"
     sudo paccache -r
     
     rm -rf ~/.local/share/Trash/files/*
 else
     printf "Aborting..."
 fi
+
+# Other tips to get more space:
+# 1 - pacman cached packages (older versions of the installed packages): 
+# go to /var/cache/pacman/pkg/
+# and delete packages that you wont use (to downgrade, for example)
+
+# 2 - journalctl logs
+# run 
+# journalctl --disk-usage
+# to check journalctl usage
+# and 
+# sudo journalctl --vacuum-size=500M
+# to clean older files enough so that the new usage is about 500M
 
 
