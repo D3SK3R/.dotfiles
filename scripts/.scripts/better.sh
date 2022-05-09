80a7e3de17abad001ea1c0a1932a3869cf374886
d3sk3r <erick.mendesoliveira@gmail.com>
2022-05-09 18:54:33 -0300

    default commit

--- scripts/.scripts/better.sh
+++ scripts/.scripts/better.sh
@@ -2,6 +2,7 @@
 
 while true;
 do
-    sleep 600
+    miuntes=30
+    sleep $((60*$minutes))
     dunstify -i "/usr/share/icons/Papirus-Dark/22x22/emblems/emblem-important.svg" -t 5000 "Are you doing something useful?" "\nIf not, now:\nStudy or Exercise" 
 done
