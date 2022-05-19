c3fda38b67f9cbf5ff96ce5b7318cd103edb9977
d3sk3r <erick.mendesoliveira@gmail.com>
2022-05-19 18:31:25 -0300

    default commit

--- scripts/.scripts/cleanup.sh
+++ scripts/.scripts/cleanup.sh
@@ -61,7 +61,7 @@ fi
 # and 
 # sudo journalctl --vacuum-size=500M
 # to clean older files enough so that the new usage is about 500M
-# you can also schedule a vacuum:
+# you can also clean files older then some time:
 # sudo journalctl --vacuum-time=10d
 
 # 4 - packages in opt
