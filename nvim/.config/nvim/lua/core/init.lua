1ee08fe432f01c77e66a5497c4fc0eeff58126e4
d3sk3r <erick.mendesoliveira@gmail.com>
2022-06-20 02:58:00 -0300

    default commit

---
+++ nvim/.config/nvim/lua/core/init.lua
@@ -0,0 +1,60 @@
+vim.cmd "silent! command! NvChadUpdate lua require('nvchad').update_nvchad()"
+
+local autocmd = vim.api.nvim_create_autocmd
+
+-- Disable statusline in dashboard
+autocmd("FileType", {
+   pattern = "alpha",
+   callback = function()
+      vim.opt.laststatus = 0
+   end,
+})
+
+autocmd("BufUnload", {
+   buffer = 0,
+   callback = function()
+      vim.opt.laststatus = 3
+   end,
+})
+
+-- Don't auto commenting new lines
+autocmd('BufEnter', {
+  pattern = '*',
+  command = 'set fo-=c fo-=r fo-=o'
+})
+
+vim.cmd [[
+    " map Q, W and X to quit, write and write+quit vim
+    :command Q q
+    :command W w
+    :command X x
+
+    noremap <C-BS> <C-w>
+    noremap <C-h> <C-w>
+
+    " map s, x and c keys to not yank when used
+    nnoremap s "_s
+    vnoremap s "_s
+
+    nnoremap x "_x
+    vnoremap x "_x
+
+    nnoremap c "_c
+    vnoremap c "_c
+
+    " prevent ci (change in ...) from yaking the selection
+    nnoremap ci "_ci
+
+    nmap i a
+
+    " Shortcut for replace command
+    nnoremap S :%s///gc<Left><Left><Left><Left>
+
+    " Move selected lines with CTRL up and down
+    xnoremap <C-Up> :move '<-2<CR>gv-gv
+    xnoremap <C-Down> :move '>+1<CR>gv-gv
+
+    " Minimal number of screen lines to keep above and below the cursor.
+    set scrolloff=5
+
+]]
